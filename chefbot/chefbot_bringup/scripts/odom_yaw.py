9566648173550e4d88f7d4ee9172f6dee8238395
roach <giraftw2002@gmail.com>
2021-06-11 16:43:44 +0800

    publish_tf: false for ekf yaml file

--- chefbot/chefbot_bringup/scripts/odom_yaw.py
+++ chefbot/chefbot_bringup/scripts/odom_yaw.py
@@ -1,20 +1,21 @@
 #!/usr/bin/env python
 import rospy
-from sensor_msgs.msg import Imu
+from nav_msgs.msg import Odometry
 from tf.transformations import euler_from_quaternion, quaternion_from_euler
 
 roll = pitch = yaw = 0.0
 
 def get_rotation (msg):
     global roll, pitch, yaw
-    orientation_q = msg.orientation
+    orientation_q = msg.pose.pose.orientation
     orientation_list = [orientation_q.x, orientation_q.y, orientation_q.z, orientation_q.w]
     (roll, pitch, yaw) = euler_from_quaternion (orientation_list)
-    print yaw
+    rospy.loginfo("odom yaw = %f", yaw)
+    #print yaw
 
-rospy.init_node('my_quaternion_to_euler')
+rospy.init_node('odom_quaternion_to_euler')
 
-sub = rospy.Subscriber ('/imu/data', Imu, get_rotation)
+sub = rospy.Subscriber ('/odom', Odometry, get_rotation)
 
 r = rospy.Rate(1)
 while not rospy.is_shutdown():
